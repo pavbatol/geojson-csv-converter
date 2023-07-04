c04065e67e7a9487e1327812c6624772d65c4456
P.Sergey <pavbatol@yandex.ru>
2023-07-04 23:37:38 +0400

    feat: Trying to run JAR with corrected POM

--- src/main/java/com/pavbatol/gjcc/App.java
+++ src/main/java/com/pavbatol/gjcc/App.java
@@ -1,17 +1,21 @@
 package com.pavbatol.gjcc;
 
 import com.pavbatol.gjcc.converter.Converter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+@Slf4j
 public class App {
     public static void main(String[] args) throws IOException {
         final Logger logger = LoggerFactory.getLogger(App.class);
-        logger.debug("App starting");
+        logger.info("App starting");
 
         Converter converter = new Converter();
         converter.run();
+
+        logger.info("The application is completed");
     }
 }
