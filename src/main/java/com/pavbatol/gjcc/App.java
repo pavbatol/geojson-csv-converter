970883467fcdbc51c8f05e4212b951358dff950a
P.Sergey <pavbatol@yandex.ru>
2023-07-11 14:25:58 +0400

    feat: Add ability to launch app with system vars, external application.properties

--- src/main/java/com/pavbatol/gjcc/App.java
+++ src/main/java/com/pavbatol/gjcc/App.java
@@ -1,5 +1,8 @@
 package com.pavbatol.gjcc;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import com.pavbatol.gjcc.config.AppConfig;
 import com.pavbatol.gjcc.converter.Converter;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -9,8 +12,17 @@ import java.io.IOException;
 
 @Slf4j
 public class App {
+
+    private static final String LOG_LEVEL = AppConfig.getInstance().getLogLevel();
+    private static final String PACKAGE_COM_PAVBATOL = "com.pavbatol";
+
     public static void main(String[] args) throws IOException {
+
         final Logger logger = LoggerFactory.getLogger(App.class);
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.getLogger(PACKAGE_COM_PAVBATOL).setLevel(Level.toLevel(LOG_LEVEL));
+
         logger.info("App starting");
 
         Converter.run();
--- src/main/java/com/pavbatol/gjcc/config/AppConfig.java
+++ src/main/java/com/pavbatol/gjcc/config/AppConfig.java
@@ -2,17 +2,33 @@ package com.pavbatol.gjcc.config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
+import java.util.Set;
 
 public class AppConfig {
     private static final Properties properties = new Properties();
-    public static final String APPLICATION_PROPERTIES = "application.properties";
+    private static final String RESOURCE_APPLICATION_PROPERTIES = "application.properties";
+    private static final String PROPERTY_APP_LOG_LEVEL = "app.log.level";
+    private static final String SYSTEM_VAR_PROPERTIES_PATH = "properties.path";
 
     private AppConfig() {
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(APPLICATION_PROPERTIES)) {
+        String propertiesPath = System.getProperty(SYSTEM_VAR_PROPERTIES_PATH);
+        try (InputStream inputStream = propertiesPath == null
+                ? ClassLoader.getSystemResourceAsStream(RESOURCE_APPLICATION_PROPERTIES)
+                : Files.newInputStream(Path.of(propertiesPath))) {
             properties.load(inputStream);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load " + APPLICATION_PROPERTIES, e);
+            throw new RuntimeException("Failed to load " + RESOURCE_APPLICATION_PROPERTIES, e);
+        }
+
+        Set<String> keys = properties.stringPropertyNames();
+        for (String key : keys) {
+            String systemProperty = System.getProperty(key);
+            if (systemProperty != null) {
+                properties.setProperty(key, systemProperty);
+            }
         }
     }
 
@@ -26,6 +42,10 @@ public class AppConfig {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public String getLogLevel() {
+        return getProperty(PROPERTY_APP_LOG_LEVEL);
     }
 
     private static class InstanceHolder {
